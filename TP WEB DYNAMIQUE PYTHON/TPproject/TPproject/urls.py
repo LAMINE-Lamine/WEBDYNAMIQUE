7a20f264322035ff379421a89d9493d9192b574a
[lamine] <lamineelias200@gmail.com>
2022-05-08 17:08:08 +0200

    premiere version

--- TP WEB DYNAMIQUE PYTHON/TPproject/TPproject/urls.py
+++ TP WEB DYNAMIQUE PYTHON/TPproject/TPproject/urls.py
@@ -18,5 +18,6 @@ from django.urls import path , include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('bibliotheque/', include("bibliotheque.urls"))
+    path('bibliotheque/', include("bibliotheque.urls")),
+    path('', include("bibliotheque.urls")),
 ]
